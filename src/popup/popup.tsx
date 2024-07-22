e2c74e863066f580632ee5ae0fc7edcf6757f341
Bart Spaans <143680439+spaansba@users.noreply.github.com>
2024-07-22 15:54:47 +0200

    webpack config

---
+++ src/popup/popup.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { createRoot } from "react-dom/client"
+const test = <p>Hello World</p>
+
+const container = document.createElement("div")
+document.body.appendChild(container)
+const root = createRoot(container)
+root.render(test)
